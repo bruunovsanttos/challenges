05981e34b1444c14eca306269d2720b367001337
bruunosanttos <bruno-vieira-santos@hotmail.com>
2025-03-07 22:09:18 -0300

    feat:adicionando escopo de salvamento de banco de dados no modelo de herois, criando o init e o retorno de json

--- models/herois.py
+++ models/herois.py
@@ -3,3 +3,27 @@
 #rank (Class S, A, B, C)
 #localização (latitude, longitude)
 #status (disponivel, em missão, Inativo)
+from extensions import banco
+
+
+class Herois(banco.Model):
+    __tablename__ = "herois"
+
+    nome = banco.Column(banco.String, primary_key= True)
+    rank = banco.Column(banco.String(10))
+    localizacao = ()#ver qual a necessidade do banco para localização
+    status = ()#criar para os tres unicos status)
+
+    def __init__(self, nome, rank, localizacao, status):
+        self.nome = nome,
+        self.rank = rank,
+        self.localizacao = localizacao
+        self.status = status
+
+    def json(self):
+        return {
+            'nome':self.nome,
+            'rank':self.rank,
+            'localizacao':self.localizacao,
+            'status':self.status
+        }
