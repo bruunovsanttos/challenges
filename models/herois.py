543f0e588559d65c71761f1445ec4a22f1042c7c
bruunosanttos <bruno-vieira-santos@hotmail.com>
2025-03-09 22:22:34 -0300

    criando funções de salvar no banco de dados e query de buscas por id de heroi e buscar por rank para usar futuramente

--- models/herois.py
+++ models/herois.py
@@ -9,12 +9,14 @@ from extensions import banco
 class Herois(banco.Model):
     __tablename__ = "herois"
 
-    nome = banco.Column(banco.String, primary_key= True)
+    id_hero = banco.Column(banco.Integer, primary_key=True)
+    nome = banco.Column(banco.String(60))
     rank = banco.Column(banco.String(10))
     localizacao = ()#ver qual a necessidade do banco para localização
     status = ()#criar para os tres unicos status)
 
-    def __init__(self, nome, rank, localizacao, status):
+    def __init__(self, id_hero, nome, rank, localizacao, status):
+        self.id_hero = id_hero,
         self.nome = nome,
         self.rank = rank,
         self.localizacao = localizacao
@@ -22,8 +24,38 @@ class Herois(banco.Model):
 
     def json(self):
         return {
-            'nome':self.nome,
-            'rank':self.rank,
-            'localizacao':self.localizacao,
-            'status':self.status
+            'id_hero': self.id_hero,
+            'nome': self.nome,
+            'rank': self.rank,
+            'localizacao': self.localizacao,
+            'status': self.status
         }
+
+    def save_hero(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def update_hero(self, nome, rank, localizacao, status):
+        self.nome = nome
+        self.rank = rank
+        self.localizacao = localizacao
+        self.status = status
+
+    @classmethod
+    def delete_hero(cls, id_hero):
+        hero = cls.find_hero(id_hero)
+        if hero:
+            banco.session.delete(hero)
+            banco.session.commit()
+
+    @classmethod
+    def find_hero(cls, id_hero):
+        hero = cls.query.filter_by(id_hero=id_hero).first()#chamando o primeiro heroi com o id
+        if hero:
+            return hero
+        return None
+
+    @classmethod
+    def buscar_por_rank(cls, rank):
+        return cls.query.filter_by(rank=rank).all()#busca herois opr rank para chamar no combate
+
